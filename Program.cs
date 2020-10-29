23874d3d5df99b4ea85511687508a9ca980bca2f
Vladimir Pikalov <vladimir.pikalov@aurea.com>
2020-10-29 19:51:04 +0700

    Console base project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace devspace2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
