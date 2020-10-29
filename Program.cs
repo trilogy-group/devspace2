46ca5ee4da97699a629aac742c0cebc41d895c40
vladimir-pikalov <vladimir.pikalov@gmail.com>
2020-10-29 13:58:33 +0000

    ext
    Signed-off-by: vladimir-pikalov <vladimir.pikalov@gmail.com>

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace devspace2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Test2");
         }
     }
 }
